447013b61f700b5f6563ce9ca0a5bb54af887003
Sean D Gillespie <sean@mistersg.net>
2022-08-19 11:01:05 -0400

    Update command to report vertices

--- app/Command/GLTF/Loader/Run.hs
+++ app/Command/GLTF/Loader/Run.hs
@@ -3,7 +3,10 @@ module Command.GLTF.Loader.Run (run) where
 import Command.GLTF.Loader.App
 import Text.GLTF.Loader
 
+import Lens.Micro
+import Linear (V3(..))
 import RIO
+import RIO.List (nub)
 
 run :: RIO App ()
 run = do
@@ -11,17 +14,54 @@ run = do
   logInfo $ "File: " <> fromString file
 
   result <- liftIO $ fromFile file
-  either reportError processGltf result
+  either reportError reportGltf result
 
 reportError :: HasLogFunc logger => Errors -> RIO logger ()
 reportError err
   = logError (display err)
   >> exitFailure
 
-processGltf :: Gltf -> RIO App ()
-processGltf gltf = do
-  let nodes = view _nodes gltf
+reportGltf :: Gltf -> RIO App ()
+reportGltf gltf = do
+  reportAsset $ gltf ^. _asset
   
-  logInfo $ "Nodes: " <> display (length nodes)
+  logInfo "" -- Blank line
+
+  reportNodes gltf
+
+reportAsset :: Asset -> RIO App ()
+reportAsset asset = do
+  logInfo $ "Generator: " <> asset ^. _assetGenerator . to (fromMaybe "Unknown") . to display
+  logInfo $ "Version: " <> asset ^. _assetVersion . to display
+
+reportNodes :: Gltf -> RIO App ()
+reportNodes gltf = do
+  let nodes = gltf ^. _nodes
+  logInfo $ "Nodes: " <> display (RIO.length nodes)
+
   forM_ nodes $ \node -> do
     logInfo $ "  Name: " <> maybe "Unknown node" display (view _nodeName node)
+
+    forM_ (node ^. _nodeMeshId) $ \meshId -> do
+      logInfo "  Mesh: "
+      forM_ (gltf ^. _meshes  ^? ix meshId) reportMesh
+        
+
+reportMesh :: Mesh -> RIO App ()
+reportMesh mesh = do
+  logInfo $ "    Name: " <> mesh ^. _meshName . to (display . fromMaybe "Unknown")
+  logInfo "    Mesh Primitives:"
+
+  forM_ (mesh ^. _meshPrimitives) $ \primitive' -> do
+    logInfo "      Vertex Positions:"
+
+    forM_ (nub $ primitive' ^. _meshPrimitivePositions) $ \position -> do
+      logInfo $ "        " <> displayV3 position
+  
+displayV3 :: Display a => V3 a -> Utf8Builder
+displayV3 (V3 x y z)
+  = "("
+  <> display x <> ", "
+  <> display y <> ", "
+  <> display z <>
+  ")"
