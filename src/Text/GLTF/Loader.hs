4f888eec2cb6e79479c1908b96ad68aaa3245ae9
Sean D Gillespie <sean@mistersg.net>
2022-11-27 15:54:00 -0500

    Refactor GLB processing

--- src/Text/GLTF/Loader.hs
+++ src/Text/GLTF/Loader.hs
@@ -19,13 +19,15 @@ import Text.GLTF.Loader.Errors
 import Text.GLTF.Loader.Glb
 import Text.GLTF.Loader.Gltf
 
+import Data.Binary.Get (ByteOffset)
 import Data.Either
+import Data.Maybe (fromJust)
 import Lens.Micro
 import RIO
 import RIO.FilePath (takeDirectory)
 import qualified Codec.GlTF as GlTF
 import qualified Codec.GLB as GLB
-import qualified RIO.Vector.Partial as Vector
+import qualified RIO.Vector as Vector
 
 -- | Load a glTF scene from a ByteString
 fromJsonByteString :: MonadUnliftIO io => ByteString -> io (Either Errors Gltf)
@@ -36,28 +38,10 @@ fromJsonFile :: MonadUnliftIO io => FilePath -> io (Either Errors Gltf)
 fromJsonFile path = liftIO (GlTF.fromFile path) >>= toGltfResult (takeDirectory path) Nothing
 
 fromBinaryFile :: MonadUnliftIO io => FilePath -> io (Either Errors Glb)
-fromBinaryFile path = do
-  res <- liftIO $ GLB.fromFile path
-  case res of
-    Left (_, err) -> pure . Left . ReadError . fromString $ err
-    Right GLB.GLB{..} -> do
-      let gltfChunk = Vector.head chunks
-          gltf = GlTF.fromChunk gltfChunk
-
-      res' <- toGltfResult "." (Just (chunks Vector.! 1)) gltf
-      pure $ over _Right Glb res'
-
+fromBinaryFile path = liftIO (GLB.fromFile path) >>= toGlbResult "."
+  
 fromBinaryByteString :: MonadUnliftIO io => ByteString -> io (Either Errors Glb)
-fromBinaryByteString input = do
-  let res = GLB.fromByteString input
-  case res of
-    Left (_, err) -> pure . Left . ReadError . fromString $ err
-    Right GLB.GLB{..} -> do
-      let gltfChunk = Vector.head chunks
-          gltf = GlTF.fromChunk gltfChunk
-
-      res' <- toGltfResult "." (Just (chunks Vector.! 1)) gltf
-      pure $ over _Right Glb res'
+fromBinaryByteString input = toGlbResult "." (GLB.fromByteString input)
 
 toGltfResult
   :: MonadUnliftIO io
@@ -67,9 +51,29 @@ toGltfResult
   -> io (Either Errors Gltf)
 toGltfResult basePath chunk res = res
   & over _Left (ReadError . fromString)
-  & traverseOf _Right toGltfResult'
-  where toGltfResult' gltf
-          = runAdapter gltf
-          <$> loadBuffers gltf chunk basePath
-          <*> loadImages gltf basePath
+  & traverseOf _Right (runGltfAdapter basePath chunk)
 
+toGlbResult
+  :: MonadUnliftIO io
+  => FilePath
+  -> Either (ByteOffset, String) GLB.GLB
+  -> io (Either Errors Glb)
+toGlbResult basePath (Right res) = processGlb basePath res
+toGlbResult _        (Left (_, err)) = pure . Left . ReadError . fromString $ err
+
+runGltfAdapter
+  :: MonadUnliftIO io
+  => FilePath
+  -> Maybe GLB.Chunk
+  -> GlTF.GlTF
+  -> io Gltf
+runGltfAdapter basePath chunk gltf = runAdapter gltf
+  <$> loadBuffers gltf chunk basePath
+  <*> loadImages gltf basePath
+
+processGlb :: MonadUnliftIO io => FilePath -> GLB.GLB -> io (Either Errors Glb)
+processGlb basePath GLB.GLB{..} = over _Right Glb <$> res
+  where gltfChunk = chunks Vector.!? 0
+        bufferChunk = chunks Vector.!? 1
+        gltf = GlTF.fromChunk (fromJust gltfChunk)
+        res = toGltfResult basePath bufferChunk gltf
