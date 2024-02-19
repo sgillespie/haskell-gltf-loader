d19f7867eec589e9d9416935fc3d4bbc880bd435
Sean D Gillespie <sean@mistersg.net>
2024-02-18 22:11:41 -0500

    build: Add a nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flakeParts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flakeParts, ... }:
+      flakeParts.lib.mkFlake { inherit inputs; } {
+        systems = nixpkgs.lib.systems.flakeExposed;
+        imports = [ inputs.haskell-flake.flakeModule ];
+
+        perSystem = { self', pkgs, ... }: {
+          haskellProjects.default = {
+            settings = {
+              gltf-codec = {
+                broken = false;
+                check = false;
+              };
+            };
+
+            devShell = {
+              enable = true;
+
+              tools = hsPkgs: {
+                cabal-install = hsPkgs.cabal-install;
+                haskell-language-server = hsPkgs.haskell-language-server;
+              };
+              hlsCheck.enable = true;
+            };
+          };
+
+          packages.default = self'.packages.gltf-loader;
+
+        };
+      };
+}
